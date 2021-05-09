6fd2dd9018bfeadf74a898b89885f6d3ed586836
yi522 <yi5@pennmedicine.upenn.edu>
2021-05-09 00:22:03 -0400

    Logistic func

--- R/Logistic.R
+++ R/Logistic.R
@@ -11,8 +11,6 @@
 #' @export
 
 Logistic <- function(l){
-  x1 <- l[, 2]
-  ll <- l[, -2]
-  lr <- glm(ind_train~.-1, data=ll, offset = x1,family = binomial(link = "logit"))
-  return(c(1, lr$coefficients))
+  lr <- glm(ind_train~.-1, data=l, family = binomial(link = "logit"))
+  return(lr$coefficients)
 }
