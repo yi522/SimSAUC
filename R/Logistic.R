18086b7a59c7141fdeaa0bca7e12a4ff1da12286
yi522 <yi5@pennmedicine.upenn.edu>
2021-05-08 21:04:20 -0400

    check_full_0508

--- R/Logistic.R
+++ R/Logistic.R
@@ -6,6 +6,6 @@
 Logistic <- function(l){
   x1 <- l[, 2]
   ll <- l[, -2]
-  lr <- glm(ind_train~.-1, data=ll, family = binomial(link = "logit"))
+  lr <- glm(ind_train~.-1, data=ll, offset = x1,family = binomial(link = "logit"))
   return(c(1, lr$coefficients))
 }
